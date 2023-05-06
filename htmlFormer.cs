e9aafc774b45bcc9d83bfcd7050782409830835f
YegorPervyi <yegor.perviy@gmail.com>
2023-05-07 00:36:44 +0300

    try to make html not uploaded with each git cycle

--- htmlFormer.cs
+++ htmlFormer.cs
@@ -14,14 +14,15 @@ class htmlFormer
         {
             sw.WriteLine("<html>");
             sw.WriteLine($"<head><title>HomePage</title><link rel=\"stylesheet\" href=\"pageStyles.css\"></head>");
+            
             sw.WriteLine("<body>");
 
+
             // Додаємо заголовок
-            sw.WriteLine($"<h2>Hello, Friend! Today is {today}</h2>");
+            sw.WriteLine("<p><?php $date = date(\"Y-m-d\"); ?></p>");
+            sw.WriteLine("<h2>Hello, Friend! Today is <?php echo $date; ?></p></h2>");
             sw.WriteLine("<div>");
-            for(int i=0; i<lines.Length;i++){
-                sw.WriteLine("<h6>" + lines[i] + "</h6>");
-            }
+            sw.WriteLine("<div><object data=\"../humanReadable/<?php echo $date; ?>/weather.txt\"></object></div>");
             sw.WriteLine("</div>");
             sw.WriteLine("</body>");
             sw.WriteLine("</html>");
